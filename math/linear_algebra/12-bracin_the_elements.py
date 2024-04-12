8592de4038825ff72b3d20e3e601e08723c452dc
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-12 16:36:50 -0400

    Making task12 4:36pm 04-12-2024

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for the function np_elementwise(mat1, mat2)."""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Function that performs element-wise addition, subtraction, multiplication,
+    and division.
+    Args:
+        - mat1: a numpy.ndarray
+        - mat2: a numpy.ndarray
+    Returns:
+        A tuple containing the element-wise sum, difference, product, and quotient,
+        respectively.
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
