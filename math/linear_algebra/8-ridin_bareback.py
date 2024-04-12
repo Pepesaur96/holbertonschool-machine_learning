7a7c540f40d413f96562fc3335fa0ffe45f0b572
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-12 15:15:01 -0400

    Making task8, remade result initialization 3:08pm 04-12-2024

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -15,7 +15,7 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
 
-    result = []
+    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
