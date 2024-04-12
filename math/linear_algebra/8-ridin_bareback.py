fa3853c6f457b39527de064917f77e4770501aeb
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-12 15:08:50 -0400

    Making task8 3:08pm 04-12-2024

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for the function mat_mul(mat1, mat2)."""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Function that performs matrix multiplication.
+    Args:
+        - mat1: a list of lists of integers/floats
+        - mat2: a list of lists of integers/floats
+    Returns:
+        A new matrix representing the multiplication of mat1 and mat2.
+        If mat1 and mat2 cannot be multiplied, return None.
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
