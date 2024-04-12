5c872a5de4c72a4a11c2a91c2f25e0e0adb124cb
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-12 16:32:45 -0400

    Making task11 4:32pm 04-12-2024

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for the function np_transpose(matrix)."""
+
+
+def np_transpose(matrix):
+    """
+    Function that transposes a 2D matrix.
+    Args:
+        - matrix: a list of lists whose transpose should be returned
+    Returns:
+        A new list of lists representing the transpose of matrix
+    """
+    return matrix.transpose()
