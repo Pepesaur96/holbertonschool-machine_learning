37e6f04a7ab8df6432859b96f13a784642bd2e64
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-15 21:03:58 -0400

    Task2 / Fixed data names and added documentation 9:03pm 04-15-2024

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
+"""Module that creates a plot for C-14"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def change_scale():
+    """
+    Function that creates a plot for C-14 decay
+    """
     x = np.arange(0, 28651, 5730)
     r = np.long(0.5)
     t = 5730
-    y = np.exp((r/t) * x)
+    y = np.exp((r / t) * x)
 
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(x, y, label='C-14 Decay')
+    plt.plot(x, y)
 
     plt.yscale('log')
 
