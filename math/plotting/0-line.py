6cb6dbc698fc861c83bd517ce4b8745ef516c6c9
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-15 20:44:58 -0400

    Task 0 / Added documentation 8:42pm 04-15-2024

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-
+"""Module that creates a plot"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Function that creates a plot with y as a line
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
