a773f80f870726d0d3ab2ff2653dd8a9dc80f463
Pepesaur96 <jjvazquez96@gmail.com>
2024-04-15 21:31:26 -0400

    Task4 9:31pm 04-15-2024

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that creates a plot for frequency"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Function that creates a plot for frequency
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black', linewidth=1.2)
+
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+
+    plt.title('Project A')
+
+    plt.show()
