92fb4be5b3f0b8b85f0f3c9d0f0ad501ba12d4db
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-05 13:38:31 -0400

    Task 16

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Module that defines a deep neural network"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class that defines a deep neural network """
+
+    def __init__(self, nx, layers):
+        """
+        Initializes the DeepNeuralNetwork instance
+        Args:
+            nx: is the number of input features
+            layers: is a list representing the number of nodes in each
+                    layer of the network
+        """
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        if (not isinstance(layers, list) or
+                not all(map(lambda x: isinstance(x, int) and x > 0, layers))):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if i == 0:
+                self.weights["W" + str(i+1)] = (np.random.randn(layers[i], nx)
+                                                * np.sqrt(2 / nx))
+            else:
+                self.weights["W" + str(i+1)] = (np.random.randn(layers[i],
+                                                                layers[i - 1])
+                                                * np.sqrt(2 / layers[i - 1]))
+            self.weights["b" + str(i+1)] = np.zeros((layers[i], 1))
