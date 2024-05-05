4386b9a2d443b568f17cd5f900579c9081c8a04f
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-05 14:44:21 -0400

    Task 22 / Fixed Alpha Float checker 2:44pm 05/05/2024

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -155,7 +155,7 @@ class DeepNeuralNetwork:
             raise TypeError("iterations must be an integer")
         if iterations < 0:
             raise ValueError("iterations must be a positive integer")
-        if not isinstance(alpha, (int, float)):
+        if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha < 0:
             raise ValueError("alpha must be positive")
