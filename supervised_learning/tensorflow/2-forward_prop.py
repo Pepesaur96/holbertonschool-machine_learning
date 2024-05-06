d6d84e114e24ebeb5ce50326a813bcb235f72805
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-06 15:23:34 -0400

    Task 2 3:23pm 05/06/2024

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """ Module that contains a function of forward propagation """
 import tensorflow.compat.v1 as tf
-tf.disable_eager_execution()
 
-# Assuming the 'create_layer' function is defined in the '1-create_layer.py' file
 create_layer = __import__('1-create_layer').create_layer
 
 
@@ -19,14 +17,10 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     Returns:
     TensorFlow tensor that represents the output of the network.
     """
-    # Initialize the input layer
-    input_data = x
-
-    # Sequentially add each layer
-    for i, size in enumerate(layer_sizes):
-        activation_func = activations[i] if i < len(activations) else None
-        input_data = create_layer(
-            input_data, size, activation_func, name=f"layer_{i}")
-
-    # The final output tensor
-    return input_data
+    for i in range(len(layer_sizes)):
+        if i == 0:
+            prediction = create_layer(x, layer_sizes[0], activations[0])
+        else:
+            prediction = create_layer(prediction, layer_sizes[i],
+                                      activations[i])
+    return prediction
