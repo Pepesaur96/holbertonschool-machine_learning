e6d272de28fd151ebff467d5ac2587d05514d9ee
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-26 14:46:54 -0400

    Task 4 / removed unused import + Task 5

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -2,7 +2,6 @@
 """This module calculates the F1 score for each class in a
 confusion matrix
 """
-import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
