6b3c10b64aea606a662501ffe7e60b4444f53eda
Pepesaur96 <jjvazquez96@gmail.com>
2024-05-31 17:38:56 -0400

    Task 6

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Pooling """
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+    Function that performs pooling on images
+    Args:
+    - images: a numpy.ndarray with shape (m, h, w, c) containing multiple
+                images
+            - m: the number of images
+            - h: the height in pixels of the images
+            - w: the width in pixels of the images
+            - c: the number of channels in the image
+    - kernel_shape: a tuple of (kh, kw) containing the kernel shape for the
+                    pooling
+            - kh: the height of the kernel
+            - kw: the width of the kernel
+    - stride: a tuple of (sh, sw)
+            - sh: the stride for the height of the image
+            - sw: the stride for the width of the image
+    - mode: a string containing either max or avg, indicating whether to
+            perform maximum or average pooling, respectively
+    Returns:
+    A numpy.ndarray containing the pooled images
+    """
+    m, hm, wm, cm = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    ch = int((hm - kh) / sh) + 1
+    cw = int((wm - kw) / sw) + 1
+    convoluted = np.zeros((m, ch, cw, cm))
+    for h in range(ch):
+        for w in range(cw):
+            square = images[:, h * sh: h * sh + kh, w * sw: w * sw + kw, :]
+            if mode == 'max':
+                insert = np.max(square, axis=(1, 2))
+            else:
+                insert = np.average(square, axis=(1, 2))
+            convoluted[:, h, w, :] = insert
+    return convoluted
