b2170ccf6d028deb0bf1d0876dd67f92d76bc176
HectorV <jjvazquez96@gmail.com>
2024-08-04 12:55:52 -0400

    Task 9 / Fixed tab on line 32

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -29,7 +29,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if kmax is None:
 	    kmax = X.shape[0]
 	
-	n, d = X.shape
+    n, d = X.shape
     log_likelihoods = []
     bics = []
     best_k = None
