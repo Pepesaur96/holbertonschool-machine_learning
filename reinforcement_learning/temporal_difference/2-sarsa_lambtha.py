dd8acf2744f38d531b8d03db0e081c65397662ef
HectorV <jjvazquez96@gmail.com>
2024-11-09 15:13:17 -0400

    Task 2 / hotfix

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+"""This module contains the function for the SARSA(λ) algorithm."""
 import numpy as np
 
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
