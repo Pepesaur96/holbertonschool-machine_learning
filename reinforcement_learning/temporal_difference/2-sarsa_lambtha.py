de7b68150a60af6152723abf824551bbbb5bedaa
HectorV <jjvazquez96@gmail.com>
2024-11-09 15:05:46 -0400

    Task 2 / hotfix

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -3,7 +3,6 @@
 the function def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1, 
 gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05): that performs SARSA(λ)
 """
-import gym
 import numpy as np
 
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
