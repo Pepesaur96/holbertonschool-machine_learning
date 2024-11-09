0f35eb72465259fb722de400244c6bc0b5a3b74b
HectorV <jjvazquez96@gmail.com>
2024-11-09 10:42:03 -0400

    Task 0 / Parameter fix

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,40 +1,59 @@
 #!/usr/bin/env python3
-"""
-Performs the Monte Carlo algorithm to estimate the value function
-"""
+"""This module contains the function for the Monte Carlo algorithm."""
 import numpy as np
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
     """
-    Args:
-        env: environment instance
-        V: numpy.ndarray of shape (s,) containing the value estimate
-        policy: function that takes in a state and returns the next action to take
-        episodes: total number of episodes to train over
-        max_steps: maximum number of steps per episode
-        alpha: learning rate
-        gamma: discount rate
-    
+    Performs the Monte Carlo algorithm for estimating the value function.
+
+    Parameters:
+        env: Environment instance.
+        V: numpy.ndarray of shape (s,) containing the value estimates.
+        policy: Function that takes a state and returns the next action
+            to take.
+        episodes: Total number of episodes to train over.
+        max_steps: Maximum number of steps per episode.
+        alpha: Learning rate.
+        gamma: Discount rate.
+
     Returns:
-        V: the updated value estimate
+        Updated value estimates V.
     """
-    # Loop over episodes
     for episode in range(episodes):
+        # reset the environment and get initial state
         state = env.reset()[0]
         episode_data = []
-        # Loop over steps
+
         for step in range(max_steps):
+            # select action based on policy
             action = policy(state)
-            next_state, reward, done, _, _ = env.step(action)
+
+            # take action
+            next_state, reward, terminated, truncated, _ = env.step(
+                action)
+
+            # Append state and reward to the episode history
             episode_data.append((state, reward))
-            if done:
+
+            if terminated or truncated:
                 break
+
+            # move to the next state
             state = next_state
-        
-        # Update the value estimate
+
         G = 0
+        episode_data = np.array(episode_data, dtype=int)
+
+        # Compute the returns for each state in the episode
         for state, reward in reversed(episode_data):
+            # calculate this episode's return
             G = reward + gamma * G
-            V[state] = V[state] + alpha * (G - V[state])
-    
+
+            # if this is a novel state
+            if state not in episode_data[:episode, 0]:
+                # Update the value function V(s)
+                V[state] = V[state] + alpha * (G - V[state])
+
     return V
