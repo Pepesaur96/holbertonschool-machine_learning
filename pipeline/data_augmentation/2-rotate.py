91941efcbbedc1211d7044bacdb705d3a6593e26
HectorV <jjvazquez96@gmail.com>
2024-12-09 12:27:30 -0400

    Task 2/pYCODE FIX

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def rotate_image(image):
     """
-    Rotates an image by 90 degrees counter-clockwise.    
+    Rotates an image by 90 degrees counter-clockwise.
     Args:
         image: A 3D tf.Tensor containing the image to rotate.
     Returns:
